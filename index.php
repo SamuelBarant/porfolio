961904bf62545868b3b22b6e0488be456d890d26
Samuel Barba Anton <95043338+Samucraft346@users.noreply.github.com>
2024-09-29 14:27:06 +0200

    modification head

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
-    include("./includes/nav.php");
-    include("./includes/header.php");
-    include("./includes/about_me.php");
-    include("./includes/skills.php");
-    include("./includes/job.php");
-    include("./includes/contact_me.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <meta name="google-site-verification" content="hEyA8pCmVZai632ZATiI2-oIW18dHu1kf4bYPQin6g4" />
+    <link rel="stylesheet" type="text/css" href="../public/css/style.css?v=2.0">
+    <link rel="shortcut icon" href="../assets/skills/favicon.ico" type="image/x-icon">
+    <meta name="description" content="Samuel's portfolio about his job">
+    <title>Porfolio - Samuel Barba</title>
+</head>
+<body>
+    <?php
+        include("./includes/nav.php");
+        include("./includes/header.php");
+        include("./includes/about_me.php");
+        include("./includes/skills.php");
+        include("./includes/job.php");
+        include("./includes/contact_me.php");
+    ?>
+</body>
+</html>
