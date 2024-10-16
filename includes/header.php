c312dbcb8336d6e7a5ca4c438ff05ca8854e2103
Samuel Barba Anton <95043338+Samucraft346@users.noreply.github.com>
2024-10-16 22:18:14 +0200

    16/10/2024-22:17 Rfresh photos of me

--- includes/header.php
+++ includes/header.php
@@ -27,12 +27,12 @@
                 </div>
             </button></a>          
         </div>
-        <img src="../assets/img/me.webp" alt="presentacion">
+        <img src="../assets/img/me.jpg" alt="presentacion">
     </main>
 </header>
 <header>
     <h3>Web developer</h3>
-    <img src="../assets/img/me.webp" alt="presentacion">
+    <img src="../assets/img/me.jpg" alt="presentacion">
     <main>
         <div>
             <h1>Samuel</h1>
@@ -42,7 +42,7 @@
                 <a href="https://www.linkedin.com/in/samuel-barba-217210275" target="_blank"><img src="../assets/skills/linkedin.svg" alt="linkedin"></a>
                 <a href="../assets/uploads/CV_Samuel_Barba.pdf" download="CV_Samuel_Barba.pdf"><img src="../assets/skills/cv.svg" alt="cv"></a>
             </div>
-            <button class="contact-btn">
+            <a href="#contactme"><button class="contact-btn">
                 Contact me
                 <div class="icon">
                     <svg
@@ -58,7 +58,7 @@
                     ></path>
                     </svg>
                 </div>
-                </button>                      
+            </button></a>                      
         </div>
     </main>
 </header>
