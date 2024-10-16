5589e5335c277b5b9576202fb67c122ceede3a78
Samuel Barba Anton <95043338+Samucraft346@users.noreply.github.com>
2024-10-16 21:53:39 +0200

    16/20/2024-21:52 Contact button refractor

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
                 <a href="https://www.linkedin.com/in/samuel-barba-217210275" target="_blank"><img src="../assets/skills/linkedin.svg" alt="linkedin"></a>
                 <a href="../assets/uploads/CV_Samuel_Barba.pdf" download="CV_Samuel_Barba.pdf"><img src="../assets/skills/cv.svg" alt="cv"></a>
             </div>
-            <button class="contact-btn">
+            <a href="#contactme"><button class="contact-btn">
                 Contact me
                 <div class="icon">
                     <svg
@@ -25,7 +25,7 @@
                     ></path>
                     </svg>
                 </div>
-            </button>                      
+            </button></a>          
         </div>
         <img src="../assets/img/me.webp" alt="presentacion">
     </main>
